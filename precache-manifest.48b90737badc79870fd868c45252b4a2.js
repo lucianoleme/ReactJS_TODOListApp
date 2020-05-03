59068adcd4ea26349dc74fe336e82b93a4e94918
lucianoleme <lucianogonzagaleme@gmail.com>
2020-05-03 00:52:57 -0300

    Updates

---
+++ precache-manifest.48b90737badc79870fd868c45252b4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ff4054f31d2bf9c8e70ed899009b08",
+    "url": "/ReactJS_TODOListApp/index.html"
+  },
+  {
+    "revision": "1cb536c993f72048a36e",
+    "url": "/ReactJS_TODOListApp/static/css/main.6b101d8a.chunk.css"
+  },
+  {
+    "revision": "c249c62a3e1b6e465ce8",
+    "url": "/ReactJS_TODOListApp/static/js/2.e0c0db58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS_TODOListApp/static/js/2.e0c0db58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cb536c993f72048a36e",
+    "url": "/ReactJS_TODOListApp/static/js/main.8a4484ea.chunk.js"
+  },
+  {
+    "revision": "5d78a88db5cbbed24f69",
+    "url": "/ReactJS_TODOListApp/static/js/runtime-main.5ae805c7.js"
+  }
+]);
